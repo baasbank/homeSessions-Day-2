fe527fe76bd1edc79d3aecb4a546a5971a99440a
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-11 15:52:26 +0100

    Requires the dependencies

---
+++ API/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var request = require('request'),
+    clear   = require('clear'),
+    figlet  = require('figlet'),
+    inquirer    = require('inquirer'),
+    chalk       = require('chalk'),
+    Spinner     = require('clui').Spinner;
